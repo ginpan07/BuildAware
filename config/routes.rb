3d883b35efda7d7197bba953ab8e020bf557e026
ginpan <zgpan7@gmail.com>
2024-04-20 16:04:51 +1000

    Added acitivities index and google font to journal scss

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,8 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   get "students/moods", to: "students#moods"
+  resources :students do
+    resources :activities
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,8 @@
 #   end
 require "faker"
 
-# classroom1 = Classroom.new(teacher_id: 1, student_id: 1)
-# classroom1.save
+classroom1 = Classroom.new(teacher_id: 1, student_id: 1)
+classroom1.save
 
 10.times do
   student1 = Student.new(
@@ -25,3 +25,9 @@ require "faker"
     puts "Error creating user: #{student1.errors.full_messages}"
   end
 end
+
+StudentActivity.create(activities: "Hug", image: "https://cdn.iconscout.com/icon/premium/png-256-thumb/hug-1488452-1259796.png", student_id: 1)
+StudentActivity.create(activities: "Walk", image: "https://cdn.iconscout.com/icon/premium/png-256-thumb/walk-109066.png", student_id: 2)
+StudentActivity.create(activities: "Dance", image: "https://cdn-icons-png.freepik.com/512/7779/7779711.png", student_id: 3)
+StudentActivity.create(activities: "Draw", image: "https://cdn-icons-png.flaticon.com/512/191/191714.png", student_id: 4)
+StudentActivity.create(activities: "Write", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2r_-qet7K5aYWPwSujqWgB7HNEp9Vmchgsxd1KbGvQw&s", student_id: 5)
