bb1d11fec0622e8d3fcd98fe78409345d61012e8
andrew hulme <andrew_hulme04@hotmail.com>
2024-04-20 12:28:49 +1000

    Started on journal page

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,6 @@
 class StudentsController < ApplicationController
+  skip_before_action :authenticate_teacher!
+  skip_before_action :authenticate_student!
   def journal
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@ Rails.application.routes.draw do
   devise_for :teachers
   devise_for :students
   root to: "pages#home"
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
+  get "students", to: "students#journal"
   # Defines the root path route ("/")
   # root "posts#index"
 end
