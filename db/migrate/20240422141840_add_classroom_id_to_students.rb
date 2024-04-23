88971440ade4cba56a1315768fd771c644edb3be
rebekauw <80230837+rebekauw@users.noreply.github.com>
2024-04-23 20:20:47 +1000

    fixing database

--- db/migrate/20240422141840_add_classroom_id_to_students.rb
+++
@@ -1,5 +0,0 @@
-class AddClassroomIdToStudents < ActiveRecord::Migration[7.1]
-  def change
-    add_reference :students, :classroom, null: false, foreign_key: true
-  end
-end
--- db/migrate/20240422142710_change_classroom_id_to_nullable_in_students.rb
+++
@@ -1,5 +0,0 @@
-class ChangeClassroomIdToNullableInStudents < ActiveRecord::Migration[7.1]
-  def change
-    change_column :students, :classroom_id, :bigint, null: true
-  end
-end
