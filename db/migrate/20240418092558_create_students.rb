f8fad9db22e1c19636ec6f26cc6a989c0fea4d6f
rebekauw <80230837+rebekauw@users.noreply.github.com>
2024-04-23 21:13:41 +1000

    one student could only belong to one classroom, database fixed'

--- db/migrate/20240418092558_create_students.rb
+++ db/migrate/20240418092558_create_students.rb
@@ -3,6 +3,7 @@ class CreateStudents < ActiveRecord::Migration[7.1]
     create_table :students do |t|
       t.string :name
       t.integer :age
+      t.references :classroom, null: false, foreign_key: true
       t.timestamps
     end
   end
