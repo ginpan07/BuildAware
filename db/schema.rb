d0e57f8e0e7ec57c68ce21db2bb0ff5abb70f8f3
rebekauw <80230837+rebekauw@users.noreply.github.com>
2024-04-25 21:40:10 +1000

    added classroom name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_18_104555) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_25_113121) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_18_104555) do
     t.bigint "teacher_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["teacher_id"], name: "index_classrooms_on_teacher_id"
   end
 
