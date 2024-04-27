b670dea0674fb3bb5aa0794964ef8b224ce146df
rebekauw <80230837+rebekauw@users.noreply.github.com>
2024-04-27 15:12:03 +1000

    fix journal naming issues

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.1].define(version: 2024_04_27_010928) do
-
+ActiveRecord::Schema[7.1].define(version: 2024_04_27_040043) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,7 +38,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_27_010928) do
   end
 
   create_table "student_activities", force: :cascade do |t|
-    t.string "activity"
     t.bigint "student_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
