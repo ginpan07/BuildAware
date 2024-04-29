7feb0deb4c5721b3970d84e6dda133da5ddf56d9
ginpan <zgpan7@gmail.com>
2024-04-29 22:51:30 +1000

    Added missing codes and deleted wrong codes in navbar for logout to be showed

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,7 @@
 class StudentsController < ApplicationController
-
+  before_action :authenticate_student!
 
   # skip_before_action :authenticate_student!
-  skip_before_action :authenticate_student!
 
   def journal
     @mood = params[:mood]
