4d69d35471c3c61075a88fd3f017627d4d324830
ginpan <zgpan7@gmail.com>
2024-05-04 17:12:25 +1000

    Fixed show

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -9,33 +9,28 @@ class ActivitiesController < ApplicationController
   end
 
   def show
-    # @stu_activity = StudentActivity.find(params[:id])
+    #@stu_activity = StudentActivity.find(params[:id])
     # if @stu_activity.present?
     #   @activity = @stu_activity.activity
+    # end
 
-
-    @activities = Activity.find(params[:id])
+    @activity = Activity.find(params[:id])
     @student_id = params[:student_id]
     @student = current_student
     #current_student.moods.create(mood: params[:mood])
 
-    if @student.present? && @activities.present?
-      check_student_activity = @student.student_activities.find_by(activity_id: @activities.id)
+    if @student.present? && @activity.present?
+      check_student_activity = @student.student_activities.find_by(activity_id: @activity.id)
       if check_student_activity.nil?
-        @student.student_activities.create(student_id: @student.id, activity_id: @activities.id)
-        @stu_activity = StudentActivity.find(params[:id])
-        if @stu_activity.present?
-          @activity = @stu_activity.activity
-        end
+        @student.student_activities.create(student_id: @student.id, activity_id: @activity.id)
+
       else
-        @student.student_activities.last.update(activity_id: @activities.id)
-        @stu_activity = StudentActivity.find(params[:id])
-        if @stu_activity.present?
-          @activity = @stu_activity.activity
-        end
+        @student.student_activities.last.update(activity_id: @activity.id)
+
       end
     else
       render "show" # Render the journal page again if student or mood is missing
     end
+
   end
 end
